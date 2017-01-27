ff67ca9894726987ae5427fc6ff0ef63cbae3b8e
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-01-27 22:14:30 +0100

    Trying to fix U

--- src/vlc/ldb/dao/transformer/UserTransformer.java
+++ src/vlc/ldb/dao/transformer/UserTransformer.java
@@ -3,8 +3,12 @@ package vlc.ldb.dao.transformer;
 import vlc.common.to.UserTO;
 import vlc.ldb.dao.EntityDAO;
 import vlc.ldb.model.User;
+import vlc.ldb.persistence.PersistenceManager;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserTransformer implements EntityTransformer<UserTO, User> {
 
@@ -27,13 +31,15 @@ public class UserTransformer implements EntityTransformer<UserTO, User> {
 
     @Override
     public void updateEntity(EntityManager entityManager, EntityDAO<UserTO, User> entityDAO, UserTO userTO) {
-        UserTO oldUserTO = entityDAO.readEntity(userTO.getId());
-        User user = toModel(oldUserTO);
-        user = entityManager.merge(user);
+        String query = "SELECT u FROM User u WHERE u.id=:arg1";
+        Map<String, Object> params = new HashMap<>();
+        params.put("arg1", userTO.getId());
+        User user = (User) PersistenceManager.instance.singleResultQuery(entityManager, query, params);
         user.setName(userTO.getName());
         user.setSurname(userTO.getSurname());
         user.setBirthday(userTO.getBirthday());
         user.setTelegramId(userTO.getTelegramId());
+        user = entityManager.merge(user);
         entityManager.persist(user);
     }
 }
